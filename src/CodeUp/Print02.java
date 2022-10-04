5c34c3e1826b368889099afa01aecb3d675e9268
sujin0607 <goweg0607@gmail.com>
2022-10-04 22:57:31 +0900

    CodeUp [1002]

--- src/CodeUp/Print02.java
+++ src/CodeUp/Print02.java
@@ -1,2 +1,7 @@
-package CodeUp;public class Print02 {
+package CodeUp;
+//CodeUp1002
+public class Print02 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
