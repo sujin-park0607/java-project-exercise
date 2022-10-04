362e2232016ebed1249df0714096c3d83af94041
sujin0607 <goweg0607@gmail.com>
2022-10-04 14:26:00 +0900

    update 거스름돈 문제

--- src/com/CoinTest.java
+++ src/com/CoinTest.java
@@ -10,28 +10,28 @@ import java.util.Scanner;
             int money = sc.nextInt();
 
             int money50000 = money/arr[0];
-            System.out.println("50000원" + money50000 + "개");
+            System.out.println("50000원" + money50000 + "장" + " 나머지는" + money%arr[0]);
 
             int money10000 = money/arr[1];
-            System.out.println("10000" + money10000 + "개");
+            System.out.println("10000" + money10000 + "장" + " 나머지는" + money%arr[1]);
 
             int money5000 = money/arr[2];
-            System.out.println("5000원" + money5000 + "개");
+            System.out.println("5000원" + money5000 + "장" + " 나머지는" + money%arr[2]);
 
             int money1000 = money/arr[3];
-            System.out.println("1000원" + money1000 + "개");
+            System.out.println("1000원" + money1000 + "장" + " 나머지는" + money%arr[3]);
 
             int money500 = money/arr[4];
-            System.out.println("500원" + money500 + "개");
+            System.out.println("500원" + money500 + "장"+ " 나머지는" + money%arr[4]);
 
             int money100 = money/arr[5];
-            System.out.println("100원" + money100 + "개");
+            System.out.println("100원" + money100 + "장" + " 나머지는" + money%arr[5]);
 
             int money50 = money/arr[6];
-            System.out.println("50원" + money50 + "개");
+            System.out.println("50원" + money50 + "장"+ " 나머지는" + money%arr[6]);
 
             int money10 = money/arr[7];
-            System.out.println("10원" + money10 + "개");
+            System.out.println("10원" + money10 + "장"+ " 나머지는" + money%arr[7]);
             }
 
 }
