31f4e0efcd4e20bab8a76cf8accfc4d5c81a36e1
sujin0607 <goweg0607@gmail.com>
2022-10-04 15:11:33 +0900

    CodeUp 1156

---
+++ src/com/FindOddEven.java
@@ -0,0 +1,17 @@
+package com;
+
+
+import java.util.Scanner;
+
+public class FindOddEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if(num % 2 == 0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+    }
+}
