a25418695f9586585f61bc1fa6b60bb9d3b162f0
sujin0607 <goweg0607@gmail.com>
2022-10-05 14:37:31 +0900

    update Calculator

--- src/java_1005/calcuation/Calculator.java
+++ src/java_1005/calcuation/Calculator.java
@@ -2,12 +2,19 @@ package java_1005.calcuation;
 
 public class Calculator {
     private NumberGenerator numberGenerator;
+    private int baseNum;
 
     public Calculator(NumberGenerator numberGenerator) {
         this.numberGenerator = numberGenerator;
+        this.baseNum = 10;
+    }
+
+    public Calculator(NumberGenerator numberGenerator, int baseNum) {
+        this.numberGenerator = numberGenerator;
+        this.baseNum = baseNum;
     }
 
     public void plus(int num) {
-        System.out.println(num + numberGenerator.generate(num));
+        System.out.println(baseNum + numberGenerator.generate(num));
     }
 }
--- src/java_1005/calcuation/CalculatorMain.java
+++ src/java_1005/calcuation/CalculatorMain.java
@@ -2,10 +2,10 @@ package java_1005.calcuation;
 
 public class CalculatorMain {
     public static void main(String[] args) {
-//        NumberGenerator numberGenerator = new SpecificNumberGenerator(); //고정숫자
-        NumberGenerator numberGenerator = new RandomNumberGenerator(); //랜덤숫자
+        NumberGenerator numberGenerator = new SpecificNumberGenerator(); //고정숫자
+//        NumberGenerator numberGenerator = new RandomNumberGenerator(); //랜덤숫자
 
-        Calculator calculator = new Calculator(numberGenerator);
+        Calculator calculator = new Calculator(numberGenerator, 20);
         calculator.plus(10);
 
 
