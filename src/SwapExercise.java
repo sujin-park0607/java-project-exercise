026ee174de34c7cf8c0f4e7846abe4a16797141f
sujin0607 <goweg0607@gmail.com>
2022-10-05 09:29:21 +0900

    A와 B의 값을 바꾸는 Swap

--- src/SwapExercise.java
+++ src/SwapExercise.java
@@ -1,5 +1,14 @@
 public class SwapExercise {
     public static void main(String[] args) {
-        System.out.println("hello");
+        int num1 = 4;
+        int num2 = 5;
+        int temp;
+
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        System.out.println(num1);
+        System.out.println(num2);
     }
 }
