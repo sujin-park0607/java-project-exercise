6ba8cbe251cf0ca2f4038df58aee33bf981a921c
sujin0607 <goweg0607@gmail.com>
2022-10-05 15:31:18 +0900

    add ListPractice

---
+++ src/java_1005/collection/LikeLion2th.java
@@ -0,0 +1,20 @@
+package java_1005.collection;
+
+import javax.naming.Name;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LikeLion2th {
+    private List<String> students = new ArrayList<>();
+
+    public LikeLion2th() {
+        Names names = new Names();
+        students = names.names();
+
+    }
+
+    //멋사자 2기 학생의 이름이 들어있는 list를 return하는 method
+    public List<String> getStudentList(){
+        return this.students;
+    }
+}
