c0c2f4dc8d059fcd310f57f826547f63fc5a82b4
sujin0607 <goweg0607@gmail.com>
2022-10-06 13:19:18 +0900

    Collection Map arpabet example

--- src/java_1006/map/CountArpabet.java
+++ src/java_1006/map/CountArpabet.java
@@ -3,6 +3,13 @@ package java_1006.map;
 import java.util.HashMap;
 
 public class CountArpabet {
+    public static boolean isAlphabet(char s1){
+        if((s1 >= 54 && s1 <=90) || (s1 >= 97 && s1 <=122) ){
+            return true;
+        }else{
+            return false;
+        }
+    }
     public static void main(String[] args) {
         String git = "https://github.com/dongyeon-0822/java-project-exercise";
         git = git.toLowerCase(); //소문자로 변환
@@ -11,7 +18,7 @@ public class CountArpabet {
         System.out.println(git.length());
 
         for(int i=0; i<git.length(); i++){
-            if(arpabetCnt.get(git.charAt(i)) == null && (int)git.charAt(i) >= 65 &&  (int)git.charAt(i) <= 122){
+            if(arpabetCnt.get(git.charAt(i)) == null && isAlphabet(git.charAt(i))){
                 arpabetCnt.put(git.charAt(i),1);
 
             }else if(arpabetCnt.get(git.charAt(i)) != null){
