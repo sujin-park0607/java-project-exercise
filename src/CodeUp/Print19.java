f35f6f2d10e4dbae0e2a61e44d52b99984327515
sujin0607 <goweg0607@gmail.com>
2022-10-06 09:30:23 +0900

    Collection List

--- src/CodeUp/Print19.java
+++ src/CodeUp/Print19.java
@@ -1,14 +1,14 @@
-package CodeUp;
-
-import java.util.Scanner;
-
-//CodeUp1018
-public class Print19 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String line = sc.nextLine();
-        String[] str = line.split(".");
-
-        System.out.printf("%d.%02d.%02d", (int)str[0], (int)str[0], (int)str[0]);
-    }
-}
+//package CodeUp;
+//
+//import java.util.Scanner;
+//
+////CodeUp1018
+//public class Print19 {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String line = sc.nextLine();
+//        String[] str = line.split(".");
+//
+//        System.out.printf("%d.%02d.%02d", (int)str[0], (int)str[0], (int)str[0]);
+//    }
+//}
---
+++ src/java_1006/collection/ListExercise.java
@@ -0,0 +1,19 @@
+package java_1006.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExercise {
+    private List<String> students;
+
+    public ListExercise(){
+        this.students = new ArrayList<>();
+        this.students.add("박수진");
+        this.students.add("박수진");
+        this.students.add("박수진");
+    }
+
+    public List<String> getStudents(){
+        return this.students;
+    }
+}
