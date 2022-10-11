3706801093953e199816d3eb098f777f07045b6a
sujin0607 <goweg0607@gmail.com>
2022-10-11 21:32:57 +0900

    CodeUP 1041~1050

---
+++ src/CodeUp/Print41.java
@@ -0,0 +1,14 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class Print41 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        int num = (int)ch + 1;
+        System.out.println((char)num);
+
+
+    }
+}
--- src/CodeUp/SumOfDigit.java
+++ src/CodeUp/SumOfDigit.java
@@ -1,16 +1,18 @@
-package CodeUp;
-
-import java.util.Scanner;
-
-public class SumOfDigit {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        int result = 0;
-        while(num > 0){
-            num = num/ 10;
-            result ++;
-        }
-        System.out.println(result);
-    }
-}
+//package CodeUp;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Scanner;
+//
+//public class SumOfDigit {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int num = sc.nextInt();
+//        List<> result = new ArrayList();
+//        while(num > 0){
+//            num = num/ 10;
+//            result += num%10;
+//        }
+//        System.out.println(result);
+//    }
+//}
