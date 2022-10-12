bf93d5be02dc27d2c48eb20b552f2243234ea3de
sujin0607 <goweg0607@gmail.com>
2022-10-12 09:41:55 +0900

    CodeUP Max02

--- src/CodeUp/Max02.java
+++ src/CodeUp/Max02.java
@@ -1,26 +1,23 @@
 package CodeUp;
 
-public class Max02 {
-    public int getMax(int[] arr){
-        int maxValue = 0;
+import java.util.Scanner;
 
-        for(int i=0; i < arr.length; i++){
-            if(maxValue < arr[i]){
-                maxValue = arr[i];
+public class Max02 {
+    public static void main(String[] args) {
+        int[] arr = new int[9];
+        int maxValue = 0;
+        int maxIndex = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=0; i<arr.length; i++){
+            int num = sc.nextInt();
+            if(maxValue < num){
+                maxValue = num;
+                maxIndex = i;
             }
         }
-        return maxValue;
-    }
-    public static void main(String[] args) {
-        int[] arr = new int[]{3, 29, 38, 12, 57, 74, 40, 85, 61};
-        Max02 max02 = new Max02();
-        int result = max02.getMax(arr);
-        if (result==85){
-            System.out.println("테스트 통과");
-        }else{
-            System.out.println("테스트 실패. result" + result);
-        }
-
-
+        System.out.println(maxValue);
+        System.out.println(maxIndex+1);
     }
 }
