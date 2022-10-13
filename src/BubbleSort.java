8a4919ea5e131e5ef80d2235fb02f2e8170d2f93
sujin0607 <goweg0607@gmail.com>
2022-10-13 10:08:23 +0900

    add BubbleSort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,36 +1,36 @@
 import java.util.Arrays;
 
 public class BubbleSort {
-//    public int[] sort(int[] arr){
-//        int temp;
-//        int min;
-//
-//        for(int i=0; i < arr.length; i++) {
-//            for (int j = i+1; j < arr.length; j++) {
-//                if (arr[j] < arr[i]) {
-//                    temp = arr[i];
-//                    arr[i] = arr[j];
-//                    arr[j] = temp;
-//                }
-//            }
-//        }
-//        return arr;
-//    }
-
     public int[] sort(int[] arr){
         int temp;
         int min;
 
-        for (int j = 1; j < arr.length; j++) {
-            if (arr[j] < arr[0]) {
-                temp = arr[0];
-                arr[0] = arr[j];
-                arr[j] = temp;
+        for(int i=0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j] < arr[i]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
             }
         }
-
         return arr;
     }
+
+//    public int[] sort(int[] arr){
+//        int temp;
+//        int min;
+//
+//        for (int j = 1; j < arr.length; j++) {
+//            if (arr[j] < arr[0]) {
+//                temp = arr[0];
+//                arr[0] = arr[j];
+//                arr[j] = temp;
+//            }
+//        }
+//
+//        return arr;
+//    }
 
     public static void main(String[] args) {
         int[] arr = new int[]{7, 2, 3, 9, 28, 11};
