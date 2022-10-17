d46f102d45f6507a14761d92fa3e945456af6498
sujin0607 <goweg0607@gmail.com>
2022-10-17 09:19:46 +0900

    update RightTriangle

--- src/Algorithms/RightTriangle.java
+++ src/Algorithms/RightTriangle.java
@@ -1,12 +1,16 @@
 package Algorithms;
 
 public class RightTriangle {
-    public static void main(String[] args) {
-        for(int i=1; i<= 4; i++){
+    public void printStars(int n){
+        for(int i=1; i <= n; i++){
             for(int j=0; j<i; j++){
                 System.out.print("* ");
             }
             System.out.println();
         }
+    }
+    public static void main(String[] args) {
+        RightTriangle triangle = new RightTriangle();
+        triangle.printStars(4);
     }
 }
