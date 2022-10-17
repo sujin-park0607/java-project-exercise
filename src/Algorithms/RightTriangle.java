3aa94b0dde2fa71dba444c07eca91502f42f417c
sujin0607 <goweg0607@gmail.com>
2022-10-17 09:34:50 +0900

    별찍기 다형성 추가

--- src/Algorithms/RightTriangle.java
+++ src/Algorithms/RightTriangle.java
@@ -1,16 +1,29 @@
 package Algorithms;
 
-public class RightTriangle {
+public class RightTriangle{
+    private String letter = "*";
+
+    public RightTriangle(){
+    }
+
+    public RightTriangle(String letter) {
+        this.letter = letter;
+    }
+
+
     public void printStars(int n){
+
         for(int i=1; i <= n; i++){
             for(int j=0; j<i; j++){
-                System.out.print("* ");
+                System.out.printf("%s ",this.letter);
             }
             System.out.println();
         }
     }
     public static void main(String[] args) {
-        RightTriangle triangle = new RightTriangle();
+        RightTriangle triangle = new RightTriangle("#");
         triangle.printStars(4);
     }
+
+
 }
