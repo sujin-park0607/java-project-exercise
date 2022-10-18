72f046ff3bb72cd4c043090c62998bfff9a0b32f
sujin0607 <goweg0607@gmail.com>
2022-10-18 09:58:10 +0900

    add Codeup1367

--- src/Algorithms/Parallelogram.java
+++ src/Algorithms/Parallelogram.java
@@ -6,11 +6,13 @@ public class Parallelogram {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        //공백
+
         for(int i=0; i<n ; i++){
+            //공백
             for(int j=0; j<n-i-1; j++){
                 System.out.print(" ");
             }
+            //사각형
             for(int k=0; k<n; k++){
                 System.out.print("*");
             }
